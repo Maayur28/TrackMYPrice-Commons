d138ef08821abaa47d158df20569012bb2396a51
Mayur Agarwal <mayuragarwal2812@gmail.com>
2024-04-21 10:53:14 +0530

    added fix

--- src/main/java/io/github/maayur28/model/SQSProducerModel.java
+++ src/main/java/io/github/maayur28/model/SQSProducerModel.java
@@ -2,17 +2,20 @@ package io.github.maayur28.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SQSProducerModel {
 
     private String requestId;
     private String productId;
+    private String search;
     private String domain;
     private String url;
 }
