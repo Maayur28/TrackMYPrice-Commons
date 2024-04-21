437d80588a3fe30776d8e9db1118306bf8e7abaf
Mayur Agarwal <mayuragarwal2812@gmail.com>
2024-04-21 07:12:04 +0530

    added fix

--- src/main/java/io/github/maayur28/model/SQSProducerModel.java
+++ src/main/java/io/github/maayur28/model/SQSProducerModel.java
@@ -5,20 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ValidationResult {
-    private List<String> errors = new ArrayList<>();
-    private boolean valid = true;
+public class SQSProducerModel {
 
-    public void addError(String error) {
-        errors.add(error);
-        valid = false;
-    }
+    private String productId;
+    private String domain;
+    private String url;
 }
